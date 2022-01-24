7f6cac2381507813afa71cd66f13e6cd0ae4ad80
Alex Paredes <alexparedesr95@gmail.com>
2022-01-23 23:01:32 -0600

    FEAT: Created function to create cards deck.

---
+++ assets/js/juego.js
@@ -0,0 +1,33 @@
+/**
+ * C = Clubs.
+ * D = Diamonds.
+ * H = Hearts.
+ * S = Spades.
+ */
+
+let deck = [];
+const tipos = [ 'C', 'D', 'H', 'S' ];
+const especiales = [ 'A', 'J', 'Q', 'K' ];
+
+const crearDeck = () => {
+
+    for( let i = 2; i <=10; i++ ) {
+        for( tipo of tipos) {
+            deck.push( i + tipo );
+        }
+    }
+
+    for( let tipo of tipos ) {
+        for( let especial of especiales ) {
+            deck.push( especial + tipo );
+        }
+    }
+
+    deck = _.shuffle(deck);
+
+    return deck;
+
+}
+
+crearDeck();
+
