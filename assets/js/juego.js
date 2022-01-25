7a1c058100aef3f5e8a1a1b3da9dccee7a5a8ff3
Alex Paredes <alexparedesr95@gmail.com>
2022-01-24 21:53:28 -0600

    FEAT: Created function to get the card value.

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -43,5 +43,14 @@ const pedirCarta = () => {
 
 }
 
-pedirCarta();
+const valorCarta = ( carta ) => {
 
+    const valor = carta.substring( 0, carta.length - 1 );
+
+    return isNaN( valor )
+        ? ( valor === 'A' ) ? 11 : 10
+        : valor * 1
+
+}
+
+valorCarta( pedirCarta() );
